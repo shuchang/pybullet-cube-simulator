36897ff5d67fba7902bebb666457f258ed57b638
chang <oliver.c.shu@gmail.com>
2022-10-12 14:54:45 +0300

    refactor: Cube class initialize with Euler orientation

--- cube_simulator.py
+++ cube_simulator.py
@@ -20,7 +20,6 @@ def initializeGUI():
     pb_client.setGravity(0, 0, -10)
     pb_client.resetDebugVisualizerCamera(cameraDistance=2, cameraYaw=20, cameraPitch=-40,
                                          cameraTargetPosition=(0, 0, 1))
-
     return pb_client
 
 
@@ -42,57 +41,46 @@ class Cube(metaclass=IterRegistry):
     def __init__(self, assemblePos, assembleOrn, physics_client):
         self._registry.append(self)
         self.assemblyPos = assemblePos
-        self.assemblyOrn = assembleOrn
-        self.phy_client = physics_client
-        self.boxId = None
+        self.assemblyOrn = p.getQuaternionFromEuler(assembleOrn)
+        self.pb_client = physics_client
         self.startPos = None
         self.startOrn = None
+        self.boxId = None
 
     def load(self, filename, startPos, startOrn):
-        self.boxId = p.loadURDF(filename, startPos, startOrn)
         self.startPos = startPos
-        self.startOrn = startOrn
+        self.startOrn = p.getQuaternionFromEuler(startOrn)
+        self.boxId = p.loadURDF(filename, self.startPos, self.startOrn)
 
     def get_pose(self):
-        return self.phy_client.getBasePositionAndOrientation(self.boxId)
+        return self.pb_client.getBasePositionAndOrientation(self.boxId)
 
     def set_assembly_pose(self):
-        if self.boxId != None:
-            self.phy_client.resetBasePositionAndOrientation(
-                self.boxId,
-                self.assemblyPos,
-                self.assemblyOrn)
+        self.pb_client.resetBasePositionAndOrientation(
+            self.boxId,
+            self.assemblyPos,
+            self.assemblyOrn)
 
     def set_target_pose(self, target_pos, target_orn):
-        self.phy_client.resetBasePositionAndOrientation(
+        self.pb_client.resetBasePositionAndOrientation(
             self.boxId,
             target_pos,
             target_orn)
 
     def reset_start_pose(self):
-        self.phy_client.resetBasePositionAndOrientation(
+        self.pb_client.resetBasePositionAndOrientation(
             self.boxId,
             self.startPos,
             self.startOrn)
 
-    def move_cube(self, direction):
-        assert direction in ["+x", "-x", "+y", "-y", "+z"]
+    def move_cube(self):
         alpha = 4
-        if direction == "+x":
-            force = alpha*np.array([1, 0, 0])
-        elif direction == "-x":
-            force = alpha*np.array([-1, 0, 0])
-        elif direction == "+y":
-            force = alpha*np.array([0, 1, 0])
-        elif direction == "-y":
-            force = alpha*np.array([0, -1, 0])
-        elif direction == "+z":
-            force = alpha*np.array([0, 0, 3])
-
-        current_pos = self.get_pose()[0]
-        self.phy_client.applyExternalForce(objectUniqueId=self.boxId, linkIndex=-1,
-                                           forceObj=force, posObj=current_pos,
-                                           flags=p.WORLD_FRAME)
+        force = alpha*np.array([0, 0, 3])
+        current_pos, _ = self.get_pose()
+        self.pb_client.applyExternalForce(
+            objectUniqueId=self.boxId, linkIndex=-1,
+            forceObj=force, posObj=current_pos,
+            flags=p.WORLD_FRAME)
 
 
 
@@ -146,8 +134,8 @@ def collision_detection(cube_class):
             cube2.set_assembly_pose()
             for _ in range(100):
                 cube1.set_assembly_pose()
-                cube2.move_cube(direction="+z")
-                time.sleep(1./120.)
+                cube2.move_cube()
+                time.sleep(1./50.)
                 p.stepSimulation()
 
             finalPos, _ = cube2.get_pose()
@@ -200,23 +188,23 @@ def stability_analysis(cube_class):
 def main():
     pb_client = initializeGUI()
 
-    cubeT = Cube([0.00, 0.00, 0.97], p.getQuaternionFromEuler([0, 0, -np.pi/2]), pb_client)
-    cubeL = Cube([-0.28, 0.05, 0.97], p.getQuaternionFromEuler([0, 0, np.pi/2]), pb_client)
-    cubeV = Cube([0.04, -0.11, 1.21], p.getQuaternionFromEuler([0, np.pi/2, np.pi]), pb_client)
-    cubeZ = Cube([-0.14, -0.10, 1.15], p.getQuaternionFromEuler([0, np.pi/2, 0]), pb_client)
-    cubeB = Cube([-0.29, -0.15, 1.25], p.getQuaternionFromEuler([-np.pi/2, 0, -np.pi/2]), pb_client)
-    cubeA = Cube([0.00, 0.15, 1.24], p.getQuaternionFromEuler([0, np.pi/2, -np.pi/2]), pb_client)
-    cubeP = Cube([-0.28, 0.15, 1.31], p.getQuaternionFromEuler([np.pi/2, 0, np.pi/2]), pb_client)
+    cubeT = Cube([0.00, 0.00, 0.97], [0, 0, -np.pi/2], pb_client)
+    cubeL = Cube([-0.28, 0.05, 0.97], [0, 0, np.pi/2], pb_client)
+    cubeV = Cube([0.04, -0.11, 1.21], [0, np.pi/2, np.pi], pb_client)
+    cubeZ = Cube([-0.14, -0.10, 1.15], [0, np.pi/2, 0], pb_client)
+    cubeB = Cube([-0.29, -0.15, 1.25], [-np.pi/2, 0, -np.pi/2], pb_client)
+    cubeA = Cube([0.00, 0.15, 1.24], [0, np.pi/2, -np.pi/2], pb_client)
+    cubeP = Cube([-0.28, 0.15, 1.31], [np.pi/2, 0, np.pi/2], pb_client)
 
     p.loadURDF("plane.urdf")
     p.loadURDF("table/table.urdf", [0, 0, -1], globalScaling=3)
-    cubeT.load("cubes/cube1.urdf", [-1.6, 1., 0.97], p.getQuaternionFromEuler([0, 0, 0]))
-    cubeL.load("cubes/cube2.urdf", [-0.9, 1., 0.97], p.getQuaternionFromEuler([0, 0, 0]))
-    cubeV.load("cubes/cube3.urdf", [-0.4, 1., 0.97], p.getQuaternionFromEuler([0, 0, 0]))
-    cubeZ.load("cubes/cube4.urdf", [0.2, 1., 0.97], p.getQuaternionFromEuler([0, 0, 0]))
-    cubeB.load("cubes/cube6.urdf", [0.8, 1., 1.], p.getQuaternionFromEuler([0, 0, 0]))
-    cubeA.load("cubes/cube7.urdf", [1.4, 1., 1.], p.getQuaternionFromEuler([0, 0, 0]))
-    cubeP.load("cubes/cube5.urdf", [2.0, 1., 1.], p.getQuaternionFromEuler([0, 0, 0]))
+    cubeT.load("cubes/cube1.urdf", [-1.6, 1.0, 0.97], [0, 0, 0])
+    cubeL.load("cubes/cube2.urdf", [-0.9, 1.0, 0.97], [0, 0, 0])
+    cubeV.load("cubes/cube3.urdf", [-0.4, 1.0, 0.97], [0, 0, 0])
+    cubeZ.load("cubes/cube4.urdf", [0.2, 1.0, 0.97], [0, 0, 0])
+    cubeB.load("cubes/cube6.urdf", [0.8, 1.0, 1.0], [0, 0, 0])
+    cubeA.load("cubes/cube7.urdf", [1.4, 1.0, 1.0], [0, 0, 0])
+    cubeP.load("cubes/cube5.urdf", [2.0, 1.0, 1.0], [0, 0, 0])
 
     # stabilize the cubes
     for _ in range(100):
@@ -224,7 +212,7 @@ def main():
         p.stepSimulation()
 
     # liaison_graph = contact_detection(Cube, 0.01)
-    # collision_map = collision_detection(Cube)
+    collision_map = collision_detection(Cube)
     stability_map = stability_analysis(Cube)
 
     # for cube in Cube:
