b15ab05d36b87eb0b214fb8fd6d3d858464a4302
chang <oliver.c.shu@gmail.com>
2022-10-13 22:36:05 +0300

    refactor: support six direction collision detection

--- cube_simulator.py
+++ cube_simulator.py
@@ -10,16 +10,17 @@ from pybullet_utils import bullet_client
 def initializeGUI():
     # pb_client = bullet_client.BulletClient(connection_mode=p.DIRECT)
     pb_client = bullet_client.BulletClient(connection_mode=p.GUI)
+    pb_client.setAdditionalSearchPath(pybullet_data.getDataPath())
 
     pb_client.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
     pb_client.configureDebugVisualizer(p.COV_ENABLE_RGB_BUFFER_PREVIEW, 0)
     pb_client.configureDebugVisualizer(p.COV_ENABLE_DEPTH_BUFFER_PREVIEW, 0)
     pb_client.configureDebugVisualizer(p.COV_ENABLE_SEGMENTATION_MARK_PREVIEW, 0)
 
-    pb_client.setAdditionalSearchPath(pybullet_data.getDataPath())
-    pb_client.setGravity(0, 0, -10)
-    pb_client.resetDebugVisualizerCamera(cameraDistance=1.8, cameraYaw=0, cameraPitch=-89,
-                                         cameraTargetPosition=(0, 0, 1))
+    pb_client.resetDebugVisualizerCamera(
+        cameraDistance=1.8,
+        cameraYaw=0, cameraPitch=-89,
+        cameraTargetPosition=(0, 0, 1))
     return pb_client
 
 
@@ -86,44 +87,96 @@ class Cube(metaclass=IterRegistry):
             flags=p.WORLD_FRAME)
 
 
+def initialCube(direction, pb_client):
+    Cube._registry = []
 
-def contact_detection(cube_class, max_distance):
-    num_cubes = len(cube_class)
-    distances = []
-    liaison_graph = np.zeros((num_cubes, num_cubes))
+    if direction == "+z":
+        cubeT = Cube([0.00, 0.00, 0.97], [0, 0, -np.pi/2], pb_client)
+        cubeL = Cube([-.28, 0.05, 0.97], [0, 0, np.pi/2], pb_client)
+        cubeZ = Cube([-.14, -.10, 1.15], [0, np.pi/2, 0], pb_client)
+        cubeV = Cube([0.04, -.11, 1.21], [0, np.pi/2, np.pi], pb_client)
+        cubeB = Cube([-.29, -.15, 1.25], [-np.pi/2, 0, -np.pi/2], pb_client)
+        cubeA = Cube([0.00, 0.15, 1.24], [0, np.pi/2, -np.pi/2], pb_client)
+        cubeP = Cube([-.28, 0.15, 1.31], [np.pi/2, 0, np.pi/2], pb_client)
+    elif direction =="-z":
+        cubeP = Cube([-.28, -.15, 1.01], [0, 0, np.pi], pb_client)
+        cubeB = Cube([-.29, 0.15, 1.07], [np.pi/2, 0, np.pi/2], pb_client)
+        cubeA = Cube([0.01, -.16, 1.07], [-np.pi/2, -np.pi/2, 0], pb_client)
+        cubeZ = Cube([-.13, 0.11, 1.17], [0, np.pi/2, 0], pb_client)
+        cubeV = Cube([0.07, 0.11, 1.10], [0, -np.pi/2, 0], pb_client)
+        cubeT = Cube([0.03, -.00, 1.35], [0, 0, -np.pi/2], pb_client)
+        cubeL = Cube([-.28, -.06, 1.35], [0, np.pi, np.pi/2], pb_client)
+    elif direction =="+y":
+        cubeB = Cube([-.29, -.15, 1.01], [0, 0, np.pi], pb_client)
+        cubeZ = Cube([-.142, -.048, 1.07], [np.pi/2, 0, np.pi/2], pb_client)
+        cubeV = Cube([.043, -.11, 1.03], [0, np.pi/2, 0], pb_client)
+        cubeT = Cube([.010, .148, 1.156], [0, -np.pi/2, -np.pi/2], pb_client)
+        cubeL = Cube([-.267, .148, 1.210], [0, -np.pi/2, np.pi/2], pb_client)
+        cubeA = Cube([-.002, -.148, 1.3], [0, np.pi, -np.pi/2], pb_client)
+        cubeP = Cube([-.294, -.203, 1.30], [np.pi/2, -np.pi/2, np.pi/2], pb_client)
+    elif direction == "-y":
+        cubeP = Cube([-.294, 0.160, 1.0], [0, 0, np.pi/2], pb_client)
+        cubeA = Cube([-.002, 0.090, 1.0], [0, 0, -np.pi/2], pb_client)
+        cubeL = Cube([-.282, -.190, 1.10], [0, np.pi/2, np.pi/2], pb_client)
+        cubeT = Cube([-.005, -.190, 1.153], [0, -np.pi/2, -np.pi/2], pb_client)
+        cubeZ = Cube([-.141, -.001, 1.26], [np.pi/2, 0, np.pi/2], pb_client)
+        cubeB = Cube([-.290, 0.115, 1.3], [0, np.pi, 0], pb_client)
+        cubeV = Cube([0.043, 0.050, 1.28], [np.pi/2, 0, -np.pi/2], pb_client)
+    elif direction == "+x":
+        cubeP = Cube([-.305, 0.175, 1.0], [0, 0, np.pi/2], pb_client)
+        cubeL = Cube([0.052, 0.125, 1.0], [-np.pi/2, 0, np.pi/2], pb_client)
+        cubeB = Cube([-.243, -.130, 1.0], [0, 0, -np.pi/2], pb_client)
+        cubeZ = Cube([-.150, -.080, 1.16], [0, 0, 0], pb_client)
+        cubeA = Cube([-.260, 0.190, 1.3], [0, np.pi, 0], pb_client)
+        cubeT = Cube([0.060, 0.030, 1.3], [np.pi/2, 0, np.pi/2], pb_client)
+        cubeV = Cube([-.195, -.088, 1.34], [0, 0, -np.pi/2], pb_client)
+    elif direction == "-x":
+        cubeT = Cube([-.20, 0.00, 1.01], [-np.pi/2, 0, np.pi/2], pb_client)
+        cubeV = Cube([0.06, -.16, 0.97], [0, 0, np.pi], pb_client)
+        cubeA = Cube([0.13, 0.13, 1.01], [0, 0, 0], pb_client)
+        cubeZ = Cube([0.01, -.12, 1.16], [0, np.pi, 0], pb_client)
+        cubeL = Cube([-.21, 0.06, 1.29], [np.pi/2, 0, np.pi/2], pb_client)
+        cubeB = Cube([0.11, -.19, 1.30], [0, -np.pi/2, -np.pi/2], pb_client)
+        cubeP = Cube([0.18, 0.11, 1.30], [np.pi/2, 0, 0], pb_client)
+
+    cubeT.load("cubes/cube1.urdf", [-1.8, 0.9, .97], [0, 0, 0])
+    cubeL.load("cubes/cube2.urdf", [-1.0, 0.9, .97], [0, 0, 0])
+    cubeV.load("cubes/cube3.urdf", [-0.4, 0.9, .97], [0, 0, 0])
+    cubeZ.load("cubes/cube4.urdf", [0.2, 0.9, .97], [0, 0, 0])
+    cubeB.load("cubes/cube6.urdf", [0.8, 0.9, 1.0], [0, 0, 0])
+    cubeA.load("cubes/cube7.urdf", [1.4, 0.9, 1.0], [0, 0, 0])
+    cubeP.load("cubes/cube5.urdf", [2.0, 0.9, 1.0], [0, 0, 0])
+    return Cube
+
+
+def test_assembly(direction, pb_client):
+    pb_client.setGravity(0, 0, -10)
+    pb_client.loadURDF("plane.urdf")
+    pb_client.loadURDF("table/table.urdf", [0,0,-1], globalScaling=3)
+    cube_class = initialCube(direction, pb_client)
+
+    # stabilize the cubes
+    for _ in range(100):
+        time.sleep(1./240.)
+        pb_client.stepSimulation()
 
     for cube in cube_class:
-        time.sleep(1.)
         cube.set_assembly_pose()
+        for _ in range(100):
+            time.sleep(1./80.)
+            pb_client.stepSimulation()
 
-    for _ in range(100):
-        time.sleep(1./24.)
-        p.stepSimulation()
-
-    for cube1 in cube_class:
-        for cube2 in cube_class:
-            closest_points = p.getClosestPoints(
-                cube1.boxId,
-                cube2.boxId,
-                distance=max_distance
-            )
-            if len(closest_points) > 0:
-                liaison_graph[cube1.boxId-2, cube2.boxId-2] = 1
-                liaison_graph[cube2.boxId-2, cube1.boxId-2] = 1
-
-    for cube in reversed(cube_class):
-        time.sleep(1.)
-        cube.reset_start_pose()
-
-    for _ in range(100):
-        time.sleep(1./24.)
-        p.stepSimulation()
-
-    return liaison_graph
+    pb_client.resetSimulation()
 
 
-def collision_detection(cube_class):
+def collision_detection(direction, pb_client):
     """Detects pairwise collision during disassembly"""
+    print(f"\n***** check collison in {direction} direction *****")
+    pb_client.setGravity(0, 0, -10)
+    pb_client.loadURDF("plane.urdf")
+    pb_client.loadURDF("table/table.urdf", [0,0,-1], globalScaling=3)
+    cube_class = initialCube(direction, pb_client)
+
     num_cubes = len(cube_class)
     collision_map = np.zeros((num_cubes, num_cubes))
 
@@ -149,14 +202,20 @@ def collision_detection(cube_class):
 
             cube2.reset_start_pose()
         cube1.reset_start_pose()
-
+    pb_client.resetSimulation()
     return collision_map
 
 
-def stability_analysis(cube_class):
+def stability_analysis(pb_client):
+    # TODO: generate node level stability matrix
     """Analyzes the self and pairwise stability of cubes"""
+    pb_client.setGravity(0, 0, -10)
+    pb_client.loadURDF("plane.urdf")
+    pb_client.loadURDF("table/table.urdf", [0,0,-1], globalScaling=3)
+    cube_class = initialCube("+z", pb_client)
+
     num_cubes = len(cube_class)
-    stability_map = np.zeros((num_cubes, num_cubes))
+    stability_matrix = np.zeros((num_cubes, num_cubes))
 
     for cube1 in cube_class:
         for cube2 in cube_class:
@@ -177,104 +236,39 @@ def stability_analysis(cube_class):
             finalPos, _ = cube2.get_pose()
             posDiff = np.array(finalPos) - np.array(cube2.assemblyPos)
             if not norm(posDiff) > 0.03:
-                stability_map[cube2.boxId - 2, cube1.boxId - 2] = 1
+                stability_matrix[cube2.boxId - 2, cube1.boxId - 2] = 1
                 print(f"cube[boxId:{cube2.boxId}] is stable with",
                       f"cube[boxId:{cube1.boxId}]'s support")
 
             cube2.reset_start_pose()
         cube1.reset_start_pose()
-
-    return stability_map
+    pb_client.resetSimulation()
+    return stability_matrix
 
 
 def main():
     pb_client = initializeGUI()
-    # +z
-    cubeT = Cube([0.00, 0.00, 0.97], [0, 0, -np.pi/2], pb_client)
-    cubeL = Cube([-.28, 0.05, 0.97], [0, 0, np.pi/2], pb_client)
-    cubeV = Cube([0.04, -.11, 1.21], [0, np.pi/2, np.pi], pb_client)
-    cubeZ = Cube([-.14, -.10, 1.15], [0, np.pi/2, 0], pb_client)
-    cubeB = Cube([-.29, -.15, 1.25], [-np.pi/2, 0, -np.pi/2], pb_client)
-    cubeA = Cube([0.00, 0.15, 1.24], [0, np.pi/2, -np.pi/2], pb_client)
-    cubeP = Cube([-.28, 0.15, 1.31], [np.pi/2, 0, np.pi/2], pb_client)
 
-    # -z
-    # cubeP = Cube([-.28, -.15, 1.01], [0, 0, np.pi], pb_client)
-    # cubeB = Cube([-.29, 0.15, 1.07], [np.pi/2, 0, np.pi/2], pb_client)
-    # cubeA = Cube([0.01, -.16, 1.07], [-np.pi/2, -np.pi/2, 0], pb_client)
-    # cubeZ = Cube([-.13, 0.11, 1.17], [0, np.pi/2, 0], pb_client)
-    # cubeV = Cube([0.07, 0.11, 1.10], [0, -np.pi/2, 0], pb_client)
-    # cubeT = Cube([0.03, -.00, 1.35], [0, 0, -np.pi/2], pb_client)
-    # cubeL = Cube([-.28, -.06, 1.35], [0, np.pi, np.pi/2], pb_client)
+    # test_assembly("+z", pb_client)
+    # test_assembly("-z", pb_client)
+    # test_assembly("+y", pb_client)
+    # test_assembly("-y", pb_client)
+    # test_assembly("+x", pb_client)
+    # test_assembly("-x", pb_client)
 
-    # +y
-    # cubeB = Cube([-.29, -.15, 1.01], [0, 0, np.pi], pb_client)
-    # cubeZ = Cube([-.142, -.048, 1.07], [np.pi/2, 0, np.pi/2], pb_client)
-    # cubeV = Cube([.043, -.11, 1.03], [0, np.pi/2, 0], pb_client)
-    # cubeT = Cube([.010, .148, 1.156], [0, -np.pi/2, -np.pi/2], pb_client)
-    # cubeL = Cube([-.267, .148, 1.210], [0, -np.pi/2, np.pi/2], pb_client)
-    # cubeA = Cube([-.002, -.148, 1.3], [0, np.pi, -np.pi/2], pb_client)
-    # cubeP = Cube([-.294, -.203, 1.30], [np.pi/2, -np.pi/2, np.pi/2], pb_client)
+    collision_matrix = np.zeros((7, 7, 6))
+    collision_matrix[:,:,0] = collision_detection("+z", pb_client)
+    collision_matrix[:,:,1] = collision_detection("-z", pb_client)
+    collision_matrix[:,:,2] = collision_detection("+z", pb_client)
+    collision_matrix[:,:,3] = collision_detection("-z", pb_client)
+    collision_matrix[:,:,4] = collision_detection("+z", pb_client)
+    collision_matrix[:,:,5] = collision_detection("-z", pb_client)
+    # TODO: generate inference matrix from collision matrices in 6 directions
 
-    # -y
-    # cubeP = Cube([-.294, .16, 1.0], [0, 0, np.pi/2], pb_client)
-    # cubeA = Cube([-.002, .09, 1.0], [0, 0, -np.pi/2], pb_client)
-    # cubeL = Cube([-.282, -.19, 1.10], [0, np.pi/2, np.pi/2], pb_client)
-    # cubeT = Cube([-.005, -.19, 1.153], [0, -np.pi/2, -np.pi/2], pb_client)
-    # cubeZ = Cube([-.141, -.001, 1.26], [np.pi/2, 0, np.pi/2], pb_client)
-    # cubeB = Cube([-.29, .115, 1.3], [0, np.pi, 0], pb_client)
-    # cubeV = Cube([.043, .050, 1.28], [np.pi/2, 0, -np.pi/2], pb_client)
-
-    # # +x
-    # cubeP = Cube([-.305, .175, 1.0], [0, 0, np.pi/2], pb_client)
-    # cubeL = Cube([.052, .125, 1.0], [-np.pi/2, 0, np.pi/2], pb_client)
-    # cubeB = Cube([-.243, -.13, 1.0], [0, 0, -np.pi/2], pb_client)
-    # cubeZ = Cube([-.15, -.08, 1.16], [0, 0, 0], pb_client)
-    # cubeA = Cube([-.26, .190, 1.3], [0, np.pi, 0], pb_client)
-    # cubeT = Cube([0.06, .03, 1.3], [np.pi/2, 0, np.pi/2], pb_client)
-    # cubeV = Cube([-.195, -.088, 1.34], [0, 0, -np.pi/2], pb_client)
-
-    # -x
-    # cubeT = Cube([-.20, 0.00, 1.01], [-np.pi/2, 0, np.pi/2], pb_client)
-    # cubeV = Cube([0.06, -.16, 0.97], [0, 0, np.pi], pb_client)
-    # cubeA = Cube([0.13, 0.13, 1.01], [0, 0, 0], pb_client)
-    # cubeZ = Cube([0.01, -.12, 1.16], [0, np.pi, 0], pb_client)
-    # cubeL = Cube([-.21, 0.06, 1.29], [np.pi/2, 0, np.pi/2], pb_client)
-    # cubeB = Cube([0.11, -.19, 1.30], [0, -np.pi/2, -np.pi/2], pb_client)
-    # cubeP = Cube([0.18, 0.11, 1.30], [np.pi/2, 0, 0], pb_client)
+    stability_matrix = stability_analysis(pb_client)
 
 
-    p.loadURDF("plane.urdf")
-    p.loadURDF("table/table.urdf", [0, 0, -1], globalScaling=3)
-    cubeT.load("cubes/cube1.urdf", [-1.8, 0.9, .97], [0, 0, 0])
-    cubeL.load("cubes/cube2.urdf", [-1.0, 0.9, .97], [0, 0, 0])
-    cubeV.load("cubes/cube3.urdf", [-0.4, 0.9, .97], [0, 0, 0])
-    cubeZ.load("cubes/cube4.urdf", [0.2, 0.9, .97], [0, 0, 0])
-    cubeB.load("cubes/cube6.urdf", [0.8, 0.9, 1.0], [0, 0, 0])
-    cubeA.load("cubes/cube7.urdf", [1.4, 0.9, 1.0], [0, 0, 0])
-    cubeP.load("cubes/cube5.urdf", [2.0, 0.9, 1.0], [0, 0, 0])
-
-    # stabilize the cubes
-    for _ in range(100):
-        time.sleep(1./240.)
-        p.stepSimulation()
-
-    # # # test assembly
-    for cube in Cube:
-        cube.set_assembly_pose()
-        for _ in range(100):
-            time.sleep(1./60.)
-            p.stepSimulation()
-        print()
-
-
-
-    collision_map = collision_detection(Cube)
-    stability_map = stability_analysis(Cube)
-    # liaison_graph = contact_detection(Cube, 0.01)
-
-
-    p.disconnect()
+    pb_client.disconnect()
 
 
 if __name__ == "__main__":
